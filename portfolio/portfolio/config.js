34773613e1f87eda1909e386a5eab5cbdf0ad650
sakshigupta122 <sakshigupta9313@gmail.com>
2025-03-19 12:40:37 +0530

    Delete portfolio/portfolio directory

--- portfolio/portfolio/config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    EMAIL_SERVICE: process.env.EMAIL_SERVICE || 'gmail',
-    EMAIL_USERNAME: process.env.EMAIL_USERNAME,
-    EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
-    PORT: process.env.PORT || 3000
-};
