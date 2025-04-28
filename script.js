e1c11f7c25dc2c554e7282963f7816aa5542d283
sakshigupta122 <sakshigupta9313@gmail.com>
2025-04-29 02:19:09 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Graphic Designer", " Web Designer"],
+    strings: ["Graphic Designer", "Web Designer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
